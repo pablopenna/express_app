87fa1181562a27f6acca5e2888bd873fb2295bf1
Propietario <p.pena@edu.uah.es>
2018-06-25 18:38:38 +0200

    Introducido plotly.

--- Esqueleto/public/js/angular/mainController.js
+++ Esqueleto/public/js/angular/mainController.js
@@ -399,4 +399,23 @@ app.controller('MainCtrl', function ($scope, $http) {
         var urlData = $scope.parseURL(url);
         return urlData['hostname']=='localhost';
     }
+
+
+    //----------------
+    $scope.testGraph = function()
+    {
+        //var TESTER = document.getElementById('tester');
+        //PLOTLY necesita recibir el id del elemento DOM que
+        //va a emplear para dibujar la gráfica.
+        //En este caso le indicaremos el id del 
+        //elemento deseado con la variable 
+        //$scope.TESTER.
+        $scope.TESTER = 'tester';
+        console.log("graph - " + $scope.TESTER);
+        Plotly.plot( $scope.TESTER, [{
+        x: [1, 2, 3, 4, 5],
+        y: [1, 2, 4, 8, 16] }], {
+        margin: { t: 0 } } );
+    }
+
 });
